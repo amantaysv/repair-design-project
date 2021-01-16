10780f94183b66635cda14a7ca692f9545daf66d
Amantay <amantay.sv@gmail.com>
2021-01-16 13:57:21 +0600

    working on fantasies

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,13 @@ $(document).ready(function () {
 
 $('ul.completed-projects__caption').on('click', 'li:not(.completed-projects__tab_active)', function () {
   $(this)
-    .addClass('completed-projects__tab_active').siblings().removeClass('completed-projects__tab_active')
-    .closest('div.completed-projects__tabs').find('div.completed-projects__content').removeClass('completed-projects__content_active').eq($(this).index()).addClass('completed-projects__content_active');
+  .addClass('completed-projects__tab_active').siblings().removeClass('completed-projects__tab_active')
+  .closest('div.completed-projects__tabs').find('div.completed-projects__content').removeClass('completed-projects__content_active').eq($(this).index()).addClass('completed-projects__content_active');
+});
+
+
+$('ul.fantasies__caption').on('click', 'li:not(.fantasies__tab_active)', function () {
+  $(this)
+    .addClass('fantasies__tab_active').siblings().removeClass('fantasies__tab_active')
+    .closest('div.fantasies__tabs').find('div.fantasies__content').removeClass('fantasies__content_active').eq($(this).index()).addClass('fantasies__content_active');
 });
